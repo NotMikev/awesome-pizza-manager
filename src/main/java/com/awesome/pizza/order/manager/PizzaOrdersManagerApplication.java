5e369453fcda032d744125809789a3a858944359
NotMikev <michele.laferrara1@gmail.com>
2025-10-28 12:56:28 +0100

    Scaffold

---
+++ src/main/java/com/awesome/pizza/order/manager/PizzaOrdersManagerApplication.java
@@ -0,0 +1,13 @@
+package com.awesome.pizza.order.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaOrdersManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaOrdersManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/awesome/pizza/order/manager/PizzaOrdersManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awesome.pizza.order.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaOrdersManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
